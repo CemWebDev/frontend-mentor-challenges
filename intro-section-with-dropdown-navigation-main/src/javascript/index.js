8d62a58398de99d96ca2f2d7d86b58b512739f5e
Cem <bcemcankaya@gmail.com>
2024-09-02 22:45:02 +0300

    update javascript

--- intro-section-with-dropdown-navigation-main/src/javascript/index.js
+++ intro-section-with-dropdown-navigation-main/src/javascript/index.js
@@ -1,25 +1,3 @@
-/* const dropdownItems = document.querySelectorAll(".dropdown-item");
-const dropdownLists = document.querySelectorAll(".dropdown-list");
-
-dropdownItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    dropdownLists.forEach((list) => {
-      list.style.opacity = "0";
-      list.style.visibility = "hidden";
-    });
-
-    const targetId = item.getAttribute("data-target");
-    const targetDropdown = document.getElementById(targetId);
-
-    if (targetDropdown) {
-      targetDropdown.style.opacity = "1"
-      targetDropdown.style.visibility = "visible"
-    }
-  });
-});
-
- */
-
 import Dropdown from "./dropdown.js";
 import Mobile from "./mobile.js";
 Dropdown();
