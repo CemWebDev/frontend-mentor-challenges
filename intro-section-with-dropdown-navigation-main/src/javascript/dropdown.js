855fedbb9dad6bb14c6a8870910a1682d85e438c
Cem <bcemcankaya@gmail.com>
2024-09-03 09:23:46 +0300

    update js

--- intro-section-with-dropdown-navigation-main/src/javascript/dropdown.js
+++ intro-section-with-dropdown-navigation-main/src/javascript/dropdown.js
@@ -1,37 +1,17 @@
-/* const Dropdown = () => {
-  const dropdownItems = document.querySelectorAll(".dropdown-item");
-  const dropdownLists = document.querySelectorAll(".dropdown-list");
-
-  dropdownItems.forEach((item) => {
-    item.addEventListener("click", () => {
-      const targetId = item.getAttribute("data-target");
-      const targetDropdown = document.getElementById(targetId);
-
-      dropdownLists.forEach((list) => {
-        list.style.opacity = "0";
-        list.style.visibility = "hidden";
-      });
-
-      if (targetDropdown.style.opacity !== "1") {
-        targetDropdown.style.opacity = "1";
-        targetDropdown.style.visibility = "visible";
-      } else {
-        targetDropdown.style.opacity = "0";
-        targetDropdown.style.visibility = "hidden";
-      }
-    });
-  });
-};
- */
-
 const Dropdown = () => {
   const dropdownItems = document.querySelectorAll(".dropdown-item");
   dropdownItems.forEach((item) => {
     item.addEventListener("click", () => {
       const targetId = item.getAttribute("data-target");
       const targetDropdown = document.getElementById(targetId);
-      
-      
+      if (targetDropdown.classList.contains("non-active")) {
+        targetDropdown.classList.add("active");
+        targetDropdown.classList.remove("non-active");
+      }
+      else {
+        targetDropdown.classList.add("non-active")
+        targetDropdown.classList.remove("active")
+      }
     });
   });
 };
