fc66829c5442f48bb56ae4abbd8a35aa926a306f
Cem <bcemcankaya@gmail.com>
2024-09-03 22:10:23 +0300

    numbering projects

---
+++ 05-intro-section-with-dropdown-navigation-main/src/javascript/index.js
@@ -0,0 +1,4 @@
+import Dropdown from "./dropdown.js";
+import Mobile from "./mobile.js";
+Dropdown();
+Mobile();
--- intro-section-with-dropdown-navigation-main/src/javascript/index.js
+++
@@ -1,4 +0,0 @@
-import Dropdown from "./dropdown.js";
-import Mobile from "./mobile.js";
-Dropdown();
-Mobile();
