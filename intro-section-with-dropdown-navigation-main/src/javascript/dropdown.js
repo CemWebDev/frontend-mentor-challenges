cce1fa3e12e22a905715f1c628e964e180642f27
Cem <bcemcankaya@gmail.com>
2024-09-03 09:36:30 +0300

    finished dropdown js

--- intro-section-with-dropdown-navigation-main/src/javascript/dropdown.js
+++ intro-section-with-dropdown-navigation-main/src/javascript/dropdown.js
@@ -1,13 +1,34 @@
 const Dropdown = () => {
   const dropdownItems = document.querySelectorAll(".dropdown-item");
+
   dropdownItems.forEach((item) => {
-    item.addEventListener("click", () => {
+    item.addEventListener("click", (event) => {
+      event.stopPropagation();
       const targetId = item.getAttribute("data-target");
       const targetDropdown = document.getElementById(targetId);
+
       if (targetDropdown.classList.contains("non-active")) {
         targetDropdown.classList.add("active");
         targetDropdown.classList.remove("non-active");
       } else {
+        targetDropdown.classList.add("non-active");
+        targetDropdown.classList.remove("active");
+      }
+    });
+
+    const targetId = item.getAttribute("data-target");
+    const targetDropdown = document.getElementById(targetId);
+    targetDropdown.addEventListener("click", (event) => {
+      event.stopPropagation();
+    });
+  });
+
+  document.addEventListener("click", () => {
+    dropdownItems.forEach((item) => {
+      const targetId = item.getAttribute("data-target");
+      const targetDropdown = document.getElementById(targetId);
+
+      if (targetDropdown.classList.contains("active")) {
         targetDropdown.classList.add("non-active");
         targetDropdown.classList.remove("active");
       }
