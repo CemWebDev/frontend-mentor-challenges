32d7e7eadae6772b95c88a9e0ac95a04dbf10b11
Cem <bcemcankaya@gmail.com>
2024-09-05 10:19:35 +0300

    using localStorage for switching theme

--- 09-todo-app-main/javascript/index.js
+++ 09-todo-app-main/javascript/index.js
@@ -1,21 +1,44 @@
-let isSun = true;
+let isSun = localStorage.getItem("theme") === "sun";
+
+if (localStorage.getItem("theme") === null) {
+  isSun = true;
+  localStorage.setItem("theme", "sun");
+}
+
 
 const themeIcon = document.querySelector("#theme-icon");
 const themeBackground = document.querySelector("#theme-background");
-
 const todoInput = document.querySelector("#todo-input");
 
-themeIcon.addEventListener("click", () => {
+const applyTheme = () => {
   if (isSun) {
-    themeIcon.src = "./images/icon-moon.svg";
-    themeBackground.src = "./images/bg-desktop-light.jpg";
+    themeIcon.src = "./images/icon-sun.svg";
+    themeBackground.src = "./images/bg-desktop-dark.jpg";
     todoInput.classList.remove("dark");
     todoInput.classList.add("light");
   } else {
-    themeIcon.src = "./images/icon-sun.svg";
-    themeBackground.src = "./images/bg-desktop-dark.jpg";
+    themeIcon.src = "./images/icon-moon.svg";
+    themeBackground.src = "./images/bg-desktop-light.jpg";
     todoInput.classList.remove("light");
     todoInput.classList.add("dark");
   }
+};
+
+themeIcon.addEventListener("click", () => {
   isSun = !isSun;
+  if (isSun) {
+    themeIcon.src = "./images/icon-sun.svg";
+    themeBackground.src = "./images/bg-desktop-dark.jpg";
+    todoInput.classList.remove("dark");
+    todoInput.classList.add("light");
+    localStorage.setItem("theme", "sun");
+  } else {
+    themeIcon.src = "./images/icon-moon.svg";
+    themeBackground.src = "./images/bg-desktop-light.jpg";
+    todoInput.classList.remove("light");
+    todoInput.classList.add("dark");
+    localStorage.setItem("theme", "moon");
+  }
 });
+
+applyTheme();
