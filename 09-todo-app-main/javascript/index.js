479ea3886c48a7e836ae0b229d08af7dafc65b3e
Cem <bcemcankaya@gmail.com>
2024-09-05 10:03:00 +0300

    dark and light input toggle

--- 09-todo-app-main/javascript/index.js
+++ 09-todo-app-main/javascript/index.js
@@ -3,13 +3,19 @@ let isSun = true;
 const themeIcon = document.querySelector("#theme-icon");
 const themeBackground = document.querySelector("#theme-background");
 
+const todoInput = document.querySelector("#todo-input");
+
 themeIcon.addEventListener("click", () => {
   if (isSun) {
     themeIcon.src = "./images/icon-moon.svg";
     themeBackground.src = "./images/bg-desktop-light.jpg";
+    todoInput.classList.remove("dark");
+    todoInput.classList.add("light");
   } else {
     themeIcon.src = "./images/icon-sun.svg";
-    themeBackground.src = "./images/bg-desktop-dark.jpg"
+    themeBackground.src = "./images/bg-desktop-dark.jpg";
+    todoInput.classList.remove("light");
+    todoInput.classList.add("dark");
   }
   isSun = !isSun;
 });
