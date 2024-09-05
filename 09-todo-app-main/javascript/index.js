e876b7e5d6506a43e9fd910b72539f57ec2c0c9c
Cem <bcemcankaya@gmail.com>
2024-09-05 09:06:26 +0300

    toggle icon and background

---
+++ 09-todo-app-main/javascript/index.js
@@ -0,0 +1,16 @@
+let isSun = true;
+
+const themeIcon = document.querySelector("#theme-icon");
+const themeBackground = document.querySelector("#theme-background");
+
+themeIcon.addEventListener("click", () => {
+  if (isSun) {
+    themeIcon.src = "./images/icon-moon.svg";
+    themeBackground.src = "./images/bg-desktop-light.jpg";
+  } else {
+    themeIcon.src = "./images/icon-sun.svg";
+    themeBackground.src = "./images/bg-desktop-dark.jpg"
+  }
+
+  isSun = !isSun;
+});
